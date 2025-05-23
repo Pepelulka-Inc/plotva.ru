2300993e9119edcce30c87eb00830150937d7247
kumaroid <113765449+kumaroid@users.noreply.github.com>
2025-05-23 13:50:06 +0300

    Hotfix/refactor structure (#26)
    * mv tracing in src/plotva/common
    
    * meow

--- src/plotva/common/tracing/tracer.py
+++ src/plotva/common/tracing/tracer.py
@@ -13,19 +13,21 @@ from fastapi import FastAPI
 
 
 class Tracer:
-    def __init__(self, service_name: str, app: FastAPI, otlp_endpoint: str = "http://alloy:4317/v1/traces"):
-        resource = Resource.create(attributes={
-            SERVICE_NAME: service_name
-        })
+    def __init__(
+        self,
+        service_name: str,
+        app: FastAPI,
+        otlp_endpoint: str = "http://alloy:4317/v1/traces",
+    ):
+        resource = Resource.create(attributes={SERVICE_NAME: service_name})
 
         tracer_provider = TracerProvider(resource=resource)
         trace.set_tracer_provider(tracer_provider)
-        
+
         otlp_exporter = OTLPSpanExporter(endpoint=otlp_endpoint, insecure=True)
 
         span_processor = BatchSpanProcessor(
-            span_exporter=otlp_exporter,
-            max_export_batch_size=1
+            span_exporter=otlp_exporter, max_export_batch_size=1
         )
         tracer_provider.add_span_processor(span_processor)
 
