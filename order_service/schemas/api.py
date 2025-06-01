faeaebc6721d1b058d3c8346cda7aa83e67bfc95
DaniilGoryunov <dgoryunov160604@gmail.com>
2025-06-01 22:41:59 +0300

    final countdown v1.1

--- order_service/schemas/api.py
+++ order_service/schemas/api.py
@@ -1,12 +1,12 @@
 from typing import List, Optional, Union
 from uuid import UUID
-from pydantic import BaseModel as PydanticBaseModel, Field
-from datetime import datetime, timedelta
+from pydantic import BaseModel as PydanticBaseModel
+from datetime import datetime
 from models import OrderStatus
 
 
 class BaseRequest(PydanticBaseModel):
-    user_id: str = Field(default="123e4567-e89b-12d3-a456-426655440000")
+    user_id: str
 
 
 class BaseResponse(PydanticBaseModel):
@@ -14,16 +14,11 @@ class BaseResponse(PydanticBaseModel):
 
 
 class CreateOrderRequest(BaseRequest):
-    product_ids_list: List[str] = Field(
-        default=[
-            "d9eebc99-9c0b-4ef8-bb6d-6bb9bd380a20",
-            "e0eebc99-9c0b-4ef8-bb6d-6bb9bd380a21",
-        ]
-    )
-    amounts: List[int] = Field(default=[1, 2])
-    order_time: datetime = Field(default=datetime.now())
-    shipped_time: datetime = Field(default=datetime.now() + timedelta(days=1))
-    address_id: str = Field(default="a6eebc99-9c0b-4ef8-bb6d-6bb9bd380a17")
+    product_ids_list: List[str]
+    amounts: List[int]
+    order_time: datetime
+    shipped_time: datetime
+    address_id: str
 
 
 class CreateOrderResponse(BaseResponse):
@@ -49,21 +44,17 @@ class DeleteOrderResponse(BaseResponse):
 
 
 class OrderEntryUpdate(PydanticBaseModel):
-    entry_id: UUID = Field(..., description="ID позиции заказа")
-    quantity: Optional[int] = Field(None, gt=0, description="Новое количество")
-    remove: Optional[bool] = Field(False, description="Флаг удаления позиции")
+    entry_id: UUID
+    quantity: Optional[int]
+    remove: Optional[bool]
 
 
 class UpdateOrderRequest(BaseRequest):
-    address_id: Optional[UUID] = Field(None, description="Новый адрес доставки")
-    shipped_date: Optional[datetime] = Field(None, description="Новая дата доставки")
-    status: Optional[OrderStatus] = Field(None, description="Новый статус заказа")
-    entries: Optional[List[OrderEntryUpdate]] = Field(
-        None, description="Список изменений позиций заказа"
-    )
-    note: Optional[str] = Field(
-        None, max_length=500, description="Дополнительная заметка к заказу"
-    )
+    address_id: Optional[UUID]
+    shipped_date: Optional[datetime]
+    status: Optional[OrderStatus]
+    entries: Optional[List[OrderEntryUpdate]]
+    note: Optional[str]
 
 
 class UpdatedOrderEntry(PydanticBaseModel):
@@ -76,7 +67,5 @@ class UpdatedOrderEntry(PydanticBaseModel):
 
 
 class UpdateOrderResponse(BaseResponse):
-    updated_fields: List[str] = Field([], description="Список обновленных полей")
-    new_status: Optional[OrderStatus] = Field(
-        None, description="Актуальный статус заказа после обновления"
-    )
+    updated_fields: List[str]
+    new_status: Optional[OrderStatus]
